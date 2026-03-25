b9ee2874180d1ce4288754788865bd52cfd44c08
NaagrajHM <Naagraj.H.M@gmail.com>
2026-03-25 17:11:22 +0530

    branch_server_v0.0.2, branch_client_v0.0.2 - Feature: client uses dynamic IP address to connect.

---
+++ branch_client_LATEST/InterfaceCore.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class InterfaceCore{
+   public:
+      virtual                          ~InterfaceCore ()     = default;
+      virtual bool                      MainFunction  (void) = 0;
+      static  unique_ptr<InterfaceCore> Instantiate   (string stringAddressIP);
+};
